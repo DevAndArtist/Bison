2ea25b3f81abc2a6f490a80bf9613660a2981a14
Adrian Zubarev <adrian.zubarev@devandartist.com>
2016-10-29 17:23:47 +0200

    Small renaming. Removed `fatallError`.

--- Sources/BinarySubtype.swift
+++ Sources/BinarySubtype.swift
@@ -13,21 +13,21 @@ public enum BinarySubtype : RawRepresentable {
     case uuidOld
     case uuid
     case md5
-    case reserved(UInt8)
     case userDefined(UInt8)
+    case systemReserved(UInt8)
     
     public var rawValue: UInt8  {
         
         switch self {
             
-        case .generic:                return 0x00
-        case .function:               return 0x01
-        case .binaryOld:              return 0x02
-        case .uuidOld:                return 0x03
-        case .uuid:                   return 0x04
-        case .md5:                    return 0x05
-        case .reserved(let value):    return value
-        case .userDefined(let value): return value
+        case .generic:                   return 0x00
+        case .function:                  return 0x01
+        case .binaryOld:                 return 0x02
+        case .uuidOld:                   return 0x03
+        case .uuid:                      return 0x04
+        case .md5:                       return 0x05
+        case .userDefined(let value):    return value
+        case .systemReserved(let value): return value
         }
     }
     
@@ -41,9 +41,8 @@ public enum BinarySubtype : RawRepresentable {
         case 0x03:        self = .uuidOld
         case 0x04:        self = .uuid
         case 0x05:        self = .md5
-        case 0x06...0x7F: self = .reserved(rawValue)
         case 0x80...0xFF: self = .userDefined(rawValue)
-        default:          fatalError("Impossible UInt8")
+        default:          self = .systemReserved(rawValue)
         }
     }
 }
